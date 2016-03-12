7f2753cf8a0fd9f6c873b261a4042cd8683568e3
Evgeny Kabardinsky <Evgeny Kabardinsky>
2016-03-12 18:43:44 +0500

    Intermediate commit

--- Control/Page/PageViewControl.xaml.cs
+++ Control/Page/PageViewControl.xaml.cs
@@ -20,10 +20,53 @@ namespace ISRMUL.Control.Page
     /// </summary>
     public partial class PageViewControl : UserControl
     {
+        public Manuscript.Project currentProject;
 
         public PageViewControl()
         {
             InitializeComponent();
+            RefreshProject();
+        }
+        public void Add(BitmapImage image, string name)
+        {
+            Page.PageControl page = new PageControl();
+            page.Image.Source = image;
+            page.PageName.Text = name;
+        }
+        public void Clear()
+        {
+            Pages.Items.Clear();
+        }
+        public void AddRange(IEnumerable<BitmapImage> images)
+        {
+            int i = 0;
+            foreach (BitmapImage image in images)
+            {
+                Add(image, "Страница № " + (++i));
+            }
+        }
+        public void RefreshProject()
+        {
+            Clear();
+            if (currentProject != null) AddRange(currentProject.Pages);
+        }
+
+        public static readonly DependencyProperty CurrentProjectProperty = DependencyProperty.Register("pageViewProject", typeof(Manuscript.Project), typeof(PageViewControl));
+        public Manuscript.Project pageViewProject
+        {
+            get
+            {
+                return this.GetValue(CurrentProjectProperty) as Manuscript.Project;
+            }
+            set
+            {
+                this.SetValue(CurrentProjectProperty, value);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,15 +21,66 @@ namespace ISRMUL
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            ProjectReady = false;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        #region event handlers
+
+        private void NewProjectMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var pages = Pages.Pages.Items;
-            pages.Add(new PageControl());
+            newProject();
         }
+
+        #endregion
+
+        #region commands
+
+        void newProject()
+        {
+            CurrentProject = new Manuscript.Project();
+            ProjectReady = true;
+        }
+
+        void CloseProject()
+        {
+            ProjectReady = false;
+            CurrentProject = null;
+        }
+
+        #endregion
+
+        #region property
+
+        public static readonly DependencyProperty CurrentProjectProperty = DependencyProperty.Register("CurrentProject", typeof(Manuscript.Project), typeof(MainWindow));
+        public Manuscript.Project CurrentProject
+        {
+            get
+            {
+                return this.GetValue(CurrentProjectProperty) as Manuscript.Project;
+            }
+            set
+            {
+                this.SetValue(CurrentProjectProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ProjectReadyProperty = DependencyProperty.Register("ProjectReady", typeof(Boolean), typeof(MainWindow), new FrameworkPropertyMetadata(false));
+        public Boolean ProjectReady
+        {
+            get
+            {
+                return (Boolean)this.GetValue(ProjectReadyProperty);
+            }
+            set
+            {
+                this.SetValue(ProjectReadyProperty, value);
+            }
+        }
+
+        #endregion
     }
 }
